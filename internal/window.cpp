4dc89446a73c0697fc3cef36d5506ce16288723f
iamAdrianIusca <iamAdrianIusca@gmail.com>
2019-11-23 12:26:09 +0200

    more work on the window class

--- internal/window.cpp
+++ internal/window.cpp
@@ -1,11 +1,38 @@
 #include "window.hpp"
 
+#include <GLFW/glfw3.h>
+
 namespace lamp
 {
-	bool Window::create(const std::string_view &title, int width, int height)
+	Window::Window() noexcept
+		: ptr(nullptr)
+	{
+	}
+
+	bool Window::create(const std::string_view &title, int width, int height) noexcept
 	{
 		ptr = glfwCreateWindow(width, height, title.data(), nullptr, nullptr);
 
 		return ptr != nullptr;
+	}
+
+	bool Window::closing() const noexcept
+	{
+		return glfwWindowShouldClose(ptr) == GLFW_TRUE;
+	}
+
+	void Window::swap() const noexcept
+	{
+		glfwSwapBuffers(ptr);
+	}
+
+	void Window::pull_events() noexcept
+	{
+		glfwPollEvents();
+	}
+
+	void Window::finish() noexcept
+	{
+		glfwTerminate();
 	}
 }
--- internal/window.hpp
+++ internal/window.hpp
@@ -1,13 +1,23 @@
 #pragma
 
-#include <GLFW/glfw3.h>
 #include <string_view>
+
+struct GLFWwindow;
 
 namespace lamp
 {
 	class Window
 	{
-		bool create(const std::string_view& title, int width, int height);
+	public:
+		Window() noexcept;
+
+		bool create(const std::string_view& title, int width, int height) noexcept;
+		bool closing() const noexcept;
+
+		static void pull_events() noexcept;
+		static void finish() noexcept;
+
+		void swap() const noexcept;
 
 		GLFWwindow* ptr;
 	};
