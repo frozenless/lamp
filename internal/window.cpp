f98dcb4a812119b9eecf6e52fcd4b25773d16405
iamAdrianIusca <iamAdrianIusca@gmail.com>
2019-11-23 11:53:12 +0200

    window create function

--- internal/window.cpp
+++ internal/window.cpp
@@ -2,4 +2,10 @@
 
 namespace lamp
 {
+	bool Window::create(const std::string_view &title, int width, int height)
+	{
+		ptr = glfwCreateWindow(width, height, title.data(), nullptr, nullptr);
+
+		return ptr != nullptr;
+	}
 }
--- internal/window.hpp
+++ internal/window.hpp
@@ -1,8 +1,14 @@
 #pragma
 
+#include <GLFW/glfw3.h>
+#include <string_view>
+
 namespace lamp
 {
 	class Window
 	{
+		bool create(const std::string_view& title, int width, int height);
+
+		GLFWwindow* ptr;
 	};
 }
