024c231691e4a5d6e23edda671bc8ac835780242
iamAdrianIusca <iamAdrianIusca@gmail.com>
2019-12-01 00:09:38 +0200

    small change in the create function

--- internal/util.hpp
+++ internal/util.hpp
@@ -2,15 +2,18 @@
 
 #include <glm/glm.hpp>
 
-using u32 = glm::u32;
+namespace lamp
+{
+	using u32 = glm::u32;
 
-using f32 = glm::f32;
-using f64 = glm::f64;
+	using f32 = glm::f32;
+	using f64 = glm::f64;
 
-using iv2 = glm::ivec2;
+	using iv2 = glm::ivec2;
 
-using v2 = glm::vec2;
-using v3 = glm::vec3;
-using v4 = glm::vec4;
+	using v2 = glm::vec2;
+	using v3 = glm::vec3;
+	using v4 = glm::vec4;
 
-using m4 = glm::mat4;
+	using m4 = glm::mat4;
+}
