4d36feff61bab0875dc36b7227c288496520fffa
iamAdrianIusca <iamAdrianIusca@gmail.com>
2020-01-18 23:34:25 +0200

    some work on the physics

--- internal/physics.cpp
+++ internal/physics.cpp
@@ -3,13 +3,43 @@
 
 namespace lamp
 {
-	btCollisionWorld::ClosestRayResultCallback Physics::ray(btCollisionWorld* world, const Ray& ray)
+	void Physics::init()
+	{
+		auto config = new btDefaultCollisionConfiguration();
+
+		_world = std::make_unique<btDiscreteDynamicsWorld>(
+				new btCollisionDispatcher(config), new btDbvtBroadphase(),
+				new btSequentialImpulseConstraintSolver(), config);
+	}
+
+	void Physics::set_renderer(debug::Renderer* renderer)
+	{
+		_world->setDebugDrawer(renderer);
+	}
+
+	btCollisionWorld::ClosestRayResultCallback Physics::ray(const Ray& ray)
 	{
 		const v3 end = ray.origin + ray.direction * 100.0f;
 
 		btCollisionWorld::ClosestRayResultCallback hit(math::from(ray.origin), math::from(end));
-		world->rayTest(math::from(ray.origin), math::from(end), hit);
+		_world->rayTest(math::from(ray.origin), math::from(end), hit);
 
 		return hit;
+	}
+
+	void Physics::add_rigidbody(btRigidBody* body)
+	{
+		_world->addCollisionObject(body);
+	}
+
+	void Physics::add_collision(btCollisionObject* object)
+	{
+		_world->addCollisionObject(object);
+	}
+
+	void Physics::update(const f32 delta_time)
+	{
+		_world->stepSimulation(delta_time, 10);
+		_world->debugDrawWorld();
 	}
 }
--- internal/physics.hpp
+++ internal/physics.hpp
@@ -1,8 +1,9 @@
 #pragma once
 
 #include "physics/ray.hpp"
+#include "physics/renderer.hpp"
 
-#include <btBulletCollisionCommon.h>
+#include <btBulletDynamicsCommon.h>
 #include <BulletCollision/NarrowPhaseCollision/btRaycastCallback.h>
 
 namespace lamp
@@ -10,6 +11,17 @@ namespace lamp
 	class Physics
 	{
 	public:
-		static btCollisionWorld::ClosestRayResultCallback ray(btCollisionWorld* world, const Ray& ray);
+		void init();
+		void update(f32 delta_time);
+
+		void add_rigidbody(btRigidBody* body);
+		void add_collision(btCollisionObject* object);
+
+		void set_renderer(debug::Renderer* renderer);
+
+		btCollisionWorld::ClosestRayResultCallback ray(const Ray& ray);
+
+	private:
+		std::unique_ptr<btDynamicsWorld> _world;
 	};
 }
