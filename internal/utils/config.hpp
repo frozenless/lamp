1df489a7a3d1e63a5e5a268057ccf1839c515c6e
iamAdrianIusca <iamAdrianIusca@gmail.com>
2020-01-24 01:02:51 +0200

    4.3 again

--- internal/utils/config.hpp
+++ internal/utils/config.hpp
@@ -4,11 +4,11 @@
 
 namespace lamp::versions
 {
-	constexpr const char glsl[19] = "#version 330 core\n";
+	constexpr const char glsl[19] = "#version 430 core\n";
 
 	namespace gl
 	{
-		constexpr u32 major = 3;
+		constexpr u32 major = 4;
 		constexpr u32 minor = 3;
 	}
 }
