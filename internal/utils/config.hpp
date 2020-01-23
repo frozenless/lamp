0d30062799b749b9a2a08d9ddbd5992ad9283203
iamAdrianIusca <iamAdrianIusca@gmail.com>
2020-01-24 00:53:15 +0200

    decrease the version of opengl

--- internal/utils/config.hpp
+++ internal/utils/config.hpp
@@ -4,11 +4,11 @@
 
 namespace lamp::versions
 {
-	constexpr const char glsl[19] = "#version 430 core\n";
+	constexpr const char glsl[19] = "#version 420 core\n";
 
 	namespace gl
 	{
 		constexpr u32 major = 4;
-		constexpr u32 minor = 3;
+		constexpr u32 minor = 2;
 	}
 }
