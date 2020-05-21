8664a8da7f2a0f551006696f4bb13ac0a53c41db
iamAdrianIusca <iamAdrianIusca@gmail.com>
2020-05-22 00:53:44 +0300

    made a basic function for the plane

---
+++ internal/engine/primitives.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "types.hpp"
+#include "physics.hpp"
+#include "math/rgb.hpp"
+
+#include <entityx/entityx.h>
+
+namespace lamp
+{
+    class Primitives
+    {
+    public:
+        static entityx::Entity create_plane(Physics& physics, entityx::EntityManager& entities, const math::rgb& color, const v3& position, const v3& normal, const v3& axes, float angle);
+    };
+}
